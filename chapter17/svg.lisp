1c307808e9634fa044b210f609030aa9f11805bf
cm-hirata.kei <hirata.kei@classmethod.jp>
2018-09-02 08:47:02 +0000

    end chapter 17.

--- chapter17/svg.lisp
+++ chapter17/svg.lisp
@@ -60,3 +60,31 @@
                cy (cdr center)
                r radius
                style (svg-style color))))
+
+(defun polygon (points color)
+  (tag polygon (points (format nil
+                               "~{~a,~a ~}"
+                               (mapcan (lambda (tp)
+                                         (list (car tp) (cdr tp)))
+                                       points))
+                       style (svg-style color))))
+
+(defun random-walk (value length)
+  (unless (zerop length)
+    (cons value
+          (random-walk (if (zerop (random 2))
+                           (1- value)
+                           (1+ value))
+                       (1- length)))))
+
+(with-open-file (*standard-output* "random_walk.svg"
+                                   :direction :output
+                                   :if-exists :supersede)
+                (svg 400 200 (loop repeat 10
+                                   do (polygon (append '((0 . 200))
+                                                       (loop for x
+                                                             for y in (random-walk 100 400)
+                                                             collect (cons x y))
+                                                       '((400 . 200)))
+                                               (loop repeat 3
+                                                     collect (random 256))))))
